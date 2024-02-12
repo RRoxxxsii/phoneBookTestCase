4d53a32096795c3ccd8e1d2e61b7f9a3c6f6fdcc
RRoxxxsii <mishabur38@gmail.com>
2024-02-12 11:27:45 +0300

    Added type hint

--- src/domain/usecases/phonebook.py
+++ src/domain/usecases/phonebook.py
@@ -11,14 +11,14 @@ class ListDataFromPhoneBook(BaseUseCase):
 
 class AddDataToPhoneBook(BaseUseCase):
 
-    def __call__(self, dto: ContactsDTO):
-        self.repo.create(dto)
+    def __call__(self, dto: ContactsDTO) -> None:
+        return self.repo.create(dto)
 
 
 class UpdateDataFromPhoneBook(BaseUseCase):
 
-    def __call__(self, dto: ContactsDTO, working_mobile: str):
-        self.repo.update(dto, working_mobile)
+    def __call__(self, dto: ContactsDTO, working_mobile: str) -> None:
+        return self.repo.update(dto, working_mobile)
 
 
 class FindDataFromPhoneBook(BaseUseCase):
@@ -32,10 +32,10 @@ class PhoneBookInteractor(BaseInteractor):
     def list_data(self) -> list[list[str]] | list[dict[str]]:
         return ListDataFromPhoneBook(self.repo)()
 
-    def add_data(self, dto: ContactsDTO):
+    def add_data(self, dto: ContactsDTO) -> None:
         return AddDataToPhoneBook(self.repo)(dto)
 
-    def update_data(self, dto: ContactsDTO, working_mobile: str):
+    def update_data(self, dto: ContactsDTO, working_mobile: str) -> None:
         return UpdateDataFromPhoneBook(self.repo)(dto, working_mobile)
 
     def find_data(self, dto: ContactsDTO) -> list:
